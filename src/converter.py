a72ef20746af97ea7eacb72796241ed120c06ded
hbyyy <qjaduddl94@gmail.com>
2020-12-12 21:35:49 +0900

    feat: init 메서드 수정

--- src/converter.py
+++ src/converter.py
@@ -3,22 +3,25 @@ import re
 
 
 class Converter:
-    def __init__(self, filepath):
+    def __init__(self, filepath, convert_to):
         self.filepath = filepath
+        self.convert_to = convert_to
         self.contents = []
         self.filename = ''
         self.extension = ''
-        self.converted_extension = ''
         self.converted_contents = ''
+
+        self.file = self._load_file()
+        self._load_contents()
 
     def _load_file(self):
         name, ext = os.path.splitext(self.filepath)
         self.filename = os.path.basename(name)
         self.extension = ext
         if ext == '.srt':
-            self.converted_extension = '.sbv'
+            self.convert_to = '.sbv'
         else:
-            self.converted_extension = '.srt'
+            self.convert_to = '.srt'
         file = open(self.filepath, 'r')
         return file
 
@@ -34,17 +37,16 @@ class Converter:
         result = re.sub(' --> ', ',', result, count=1)
         return result
 
-    def _convert_str_to_sbv(self):
+    def _convert_srt_to_sbv(self):
         convert = list(map(self._convert_block_srt_to_sbv, self.contents))
         self.converted_contents = '\n\n'.join(convert)
 
     def convert(self, converted_file_name, converted_file_path=None):
-        self._load_contents()
-        self._convert_str_to_sbv()
+        self._convert_srt_to_sbv()
         if converted_file_path:
-            save_path = f'{converted_file_path}/{converted_file_name}{self.converted_extension}'
+            save_path = f'{converted_file_path}/{converted_file_name}{self.convert_to}'
         else:
-            save_path = f'{converted_file_name}{self.converted_extension}'
+            save_path = f'{converted_file_name}{self.convert_to}'
 
         with open(save_path, 'w') as f:
             f.write(self.converted_contents)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from src.converter import Converter
 
 if __name__ == '__main__':
-    converter = Converter('../testsubfiles/testsub.srt')
+    converter = Converter('../testsubfiles/testsub.srt', convert_to='sbv')
     converter.convert(converted_file_name='converttestfile', converted_file_path='..')
